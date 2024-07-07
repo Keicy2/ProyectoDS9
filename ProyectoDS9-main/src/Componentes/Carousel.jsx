e354fcf7dc657162f183cb919ea0ad24690ab174
rguadamuzzz <guadamuz.rosa@gmail.com>
2024-07-06 22:47:58 -0500

    carrusel

--- ProyectoDS9-main/src/Componentes/Carousel.jsx
+++ ProyectoDS9-main/src/Componentes/Carousel.jsx
@@ -56,21 +56,32 @@ const Carrusel = () => {
                 <div className="carousel-item">
                     <img src="\utiles\Goma en Barra Rosado Pastel 10g 0.60.png" alt="Goma rosa" />
                     <h3>Goma en barra</h3>
+                    <h4> B/. 0.60</h4>
                     <button>Clic Aquí</button>
                 </div>
                 <div className="carousel-item">
                     <img src="\utiles\Lápices de Colores Caja 24 Piezas  4.95.png" alt="lapiz de color" />
                     <h3>Lápices de colores</h3>
+                    <h4> B/. 4.95</h4>
                     <button>Clic Aquí</button>
                 </div>
                 <div className="carousel-item">
                     <img src="\utiles\Libreta Acrylic Pad 10 Hojas 9 x12 5.95.png" alt="Libreta" />
                     <h3>Libreta Acrylic</h3>
+                    <h4> B/. 5.95</h4>
                     <button>Clic Aquí</button>
                 </div>
                 <div className="carousel-item">
                     <img src="\utiles\Piensa Ciencias Naturales 6 23.00.png" alt="libro" />
                     <h3>Piensa Ciensas Naturales 6</h3>
+                    <h4> B/. 23.00</h4>
+                    <button>Clic Aquí</button>
+                </div>
+
+                <div className="carousel-item">
+                    <img src="\utiles\Tijera de Oficina 8.5 1.99.png" alt="tijeras" />
+                    <h3>Tijeras de oficina</h3>
+                    <h4> B/. 1.99</h4>
                     <button>Clic Aquí</button>
                 </div>
             </Slider>
