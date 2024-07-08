5a6ec8ef391b0d5c7c6a1a39634540bf7f94a603
rguadamuzzz <guadamuz.rosa@gmail.com>
2024-07-08 10:49:04 -0500

    KITS LISTO

--- ProyectoDS9-main/src/Componentes/Kits.jsx
+++ ProyectoDS9-main/src/Componentes/Kits.jsx
@@ -11,14 +11,13 @@ const Kits = ({children}) => {
   const [cart, addToCart] = useContext(cartcontex);
 
   const products = [
-    { id: 1110, name: 'KIT ESCOLAR PRIMARIA', description: 'Escuela El Buen Pastor', price: 26.99, image: 'utiles/kit1.jpeg' },
-    { id: 2110, name: 'CUADERNO COLOR COSIDO', description: 'Cosido Raya Ancha 100 Hojas', price: 1.21, image: 'utiles/kit2.jpeg' },
-    { id: 3110, name: 'BORRADOR DE ESCOBILLA', description: 'Tamaño Grande', price: 3.95, image: 'utiles/kit3.jpeg' },
-    { id: 4110, name: 'CUADERNO COSIDO DISNEY', description: '160 páginas', price: 3.40, image: 'utiles/kit4.jpeg' },
-    { id: 5110, name: 'CUADERNO COSIDO KIUT', description: 'Tamaño grande', price: 3.95, image: 'utiles/kit5.jpeg' },
-    { id: 6110, name: 'CUADERNO COSIDO RAYA ANCHA', description: '160 páginas', price: 3.40, image: 'utiles/kit6.jpeg.png' },
-    { id: 7110, name: 'CUADERNO COSIDO', description: '50 páginas', price: 1.53, image: 'utiles/kit7.jpeg' },
-    { id: 8110, name: 'CUADERNO DE DIBUJO', description: 'Tamaño: 8.5x13', price: 3.95, image: 'utiles/kit8.jpeg' },
+    { id: 1110, name: 'KIT ESCOLAR PRIMARIA 3ER Grado', description: 'Escuela El Buen Pastor', price: 26.99, image: 'utiles/kit1.jpeg' },
+    { id: 3110, name: 'KIT ESCOLAR KINDER', description: 'Centro Bilingue Nuevo Arraiján', price: 35.99, image: 'utiles/kit3.jpeg' },
+    { id: 4110, name: 'KIT ESCOLAR PRIMARIA 1ER GRADO', description: 'Centro Bilingue Nuevo Arraiján', price: 22.60, image: 'utiles/kit4.jpeg' },
+    { id: 5110, name: 'KIT ESCOLAR NOVENO GRADO', description: 'Colegio SAINT MARY', price: 49.00, image: 'utiles/kit5.jpeg' },
+    { id: 6110, name: 'KIT ESCOLAR 4TO GRADO', description: 'Colegio SAINT MARY', price: 25.65, image: 'utiles/kit6.jpeg' },
+    { id: 7110, name: 'KIT ESCOLAR 2DO GRADO', description: 'Centro Bilingue Vista Alegre', price: 70.00, image: 'utiles/kit7.jpeg' },
+    { id: 8110, name: 'KIT ESCOLAR 5TO GRADO', description: 'Centro Bilingue Vista Alegre', price: 40.96, image: 'utiles/kit8.jpeg' },
     // Agrega otros productos según sea necesario
   ];
 
@@ -34,7 +33,7 @@ const Kits = ({children}) => {
   return (
     <div>
       <Navegacion/>
-     <h2 className="catalogo-title">Borradores y Sacapuntas</h2>
+     <h2 className="catalogo-title">KITS escolares</h2>
       <div className="catalogo-container">
         
       <div className='catalogo-filters'>
